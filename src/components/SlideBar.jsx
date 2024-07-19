f049c67c2c4c840bb271b07e3fbc20d5f3880ee2
Hamza Akbar <126912863+hamzaakbar4018@users.noreply.github.com>
2024-07-19 17:00:04 +0500

    Update SlideBar.jsx

--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -9,34 +9,34 @@ import { MdManageAccounts } from "react-icons/md";
 
 const SlideBar = () => {
   return (
-    <div className='w-56 rounded-xl'>
-      <ul className='flex flex-col border-r mt-12 border-gray-300  h-screen rounded'>
+    <div className='w-full md:w-56 rounded-xl'>
+      <ul className='flex flex-col border-r mt-12 border-gray-300 h-full md:h-screen rounded'>
         <li className='flex mt-3 items-center ml-5 justify-start'>
-          <TfiDashboard className='mr-2 font-bold'/>
+          <TfiDashboard className='mr-2'/>
           <span className='font-bold'>Dashboard</span>
         </li>
         <li className='flex mt-8 items-center ml-5 justify-start'>
-          <CiViewList className='mr-2 font-bold'/>
+          <CiViewList className='mr-2'/>
           <span className='font-bold'>Order</span>
         </li>
         <li className='flex mt-8 items-center ml-5 justify-start'>
-          <GoHistory className='mr-2 font-bold'/>
+          <GoHistory className='mr-2'/>
           <span className='font-bold'>History</span>
         </li>
         <li className='flex mt-8 items-center ml-5 justify-start'>
-          <FaPenToSquare className='mr-2 font-bold'/>
+          <FaPenToSquare className='mr-2'/>
           <span className='font-bold'>Generate Idea's</span>
         </li>
         <li className='flex mt-8 items-center ml-5 justify-start'>
-          <LuBoxes className='mr-2 font-bold'/>
+          <LuBoxes className='mr-2'/>
           <span className='font-bold'>Subscription</span>
         </li>
         <li className='flex mt-8 items-center ml-5 justify-start'>
-          <BiMessageRoundedError className='mr-2 font-bold'/>
+          <BiMessageRoundedError className='mr-2'/>
           <span className='font-bold'>FAQ</span>
         </li>
         <li className='flex mt-8 items-center ml-5 justify-start'>
-          <MdManageAccounts className='mr-2 font-bold'/>
+          <MdManageAccounts className='mr-2'/>
           <span className='font-bold'>Manage</span>
         </li>
       </ul>
